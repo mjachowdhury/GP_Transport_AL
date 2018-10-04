e3925262a1d6dd21abf560843db5b6dc949d2430
mjachowdhury <study.computing2016@gmail.com>
2018-10-04 18:29:23 +0100

    Added VehicleMapActivity.java to get the acurate location and update every sec  - onMapReady method updated setMyLocationEnabled  - Created builGoogleApiClient() method  -created onLocationChanged() method and its connected with camera to get the position

--- app/src/main/java/com/example/mohammed/transportal/VehicleMapActivity.java
+++ app/src/main/java/com/example/mohammed/transportal/VehicleMapActivity.java
@@ -1,8 +1,19 @@
 package com.example.mohammed.transportal;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -10,9 +21,12 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class VehicleMapActivity extends FragmentActivity implements OnMapReadyCallback {
+public class VehicleMapActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
 
     private GoogleMap mMap;
+    GoogleApiClient mGoogleApiCLient;
+    Location mLastLocation;
+    LocationRequest mLocationRequest;
 
     /**
      * Here we getting the maps info from the google it was given by google
@@ -42,11 +56,94 @@ public class VehicleMapActivity extends FragmentActivity implements OnMapReadyCa
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        //LatLng sydney = new LatLng(-34, 151);
+        // this section was to find the cork location.
 
+        /*// Add a marker in Sydney and move the camera
+        //LatLng sydney = new LatLng(-34, 151);
         LatLng cork = new LatLng(51.903614, -8.468399);
         mMap.addMarker(new MarkerOptions().position(cork).title("Marker in Cork"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(cork));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(cork));*/
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        buildGoogleApiClient(); // this method has to be above the mMap.setMyLocationEnabled
+
+        mMap.setMyLocationEnabled(true);
+    }
+
+    /**
+     * These below function will be used in different stages in the app when they are being used
+     * @param location
+     */
+
+    /**
+     * this function will connect with google map api
+     */
+    protected synchronized void buildGoogleApiClient(){
+        mGoogleApiCLient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        mGoogleApiCLient.connect();
+
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        mLastLocation = location;
+
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));// it can be 1 - 21
+
+    }
+
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(1000); // that means 1 sec
+        mLocationRequest.setFastestInterval(1000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);//that will drain the battery fast
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiCLient, mLocationRequest, this);
+
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
